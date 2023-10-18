35482e9ece3b821934b72e27e84f278dcfa81bf4
Sue-Hyeong Lee <hermi6143@naver.com>
2023-10-18 19:24:23 +0900

    main.py in sum_of_squares_of_even branch

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ def even_list(int_list) :
 
 # Skeleton code for sum_of_squares_of_even
 def sum_of_squares_of_even(even_int_list):
-  pass
+  sum = 0
+  for i in even_int_list:
+    sum += i**2
+  return sum
 
 def main():
   int_list = {1,2,3,4,5,6,7,8,9,10}
